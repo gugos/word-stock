aff7c30b8f158d9038301286cdf318f263fb2cb3
Gurgen <gurgenengibaryan@gmail.com>
2019-11-11 16:40:00 +0300

    Extended db_manager functionality.
    Implemented update/delete to interact with database.
    Modified on_button_find_clicked and on_button_save_clicked methods.
    Implemented local search of word before calling meaning method.
    Excluded readonly state for definition Tk.Text widget.

--- db_manager.py
+++ db_manager.py
@@ -14,14 +14,27 @@ class DBManager:
             connection.commit()
         return query_result
 
-    def get_records(self, letter):
-        table_name = letter
+    def get_records(self, table_name):
         query = f'SELECT * FROM {table_name} ORDER BY word desc'
         return self.execute_db_query(query)
 
     def save_record(self, table_name, parameters):
         query = f'INSERT INTO {table_name} VALUES(?, ?, ?, ?)'
         self.execute_db_query(query, parameters)
+
+    def update_record(self, table_name, word, parameters):
+        query = f'UPDATE {table_name} SET definition=?, additional_info=?, score=? WHERE word=\'{word}\''
+        self.execute_db_query(query, parameters)
+
+    def delete_record(self, table_name, word):
+        query = f'DELETE FROM {table_name} WHERE word=\'{word}\''
+        self.execute_db_query(query)
+
+    def record_exists(self, table_name, word):
+        query = f'SELECT COUNT(1) FROM {table_name} WHERE word=\'{word}\''
+        result = self.execute_db_query(query)
+        for record in result:
+            return True if 1 in record else False
 
     # def create_tables(self):
     #     import string
--- view.py
+++ view.py
@@ -8,7 +8,7 @@ import string
 class View(Tk):
     def __init__(self, controller):
         Tk.__init__(self)
-        self.definition = Text(self, wrap='word', state='disabled')
+        self.definition = Text(self, wrap='word')
         self.additional_info = Text(self, wrap='word')
         self.textvariable = StringVar()
         self.controller = controller
@@ -22,14 +22,22 @@ class View(Tk):
         Label(self, text='Find word definition:').grid(row=0, sticky='w')
         Entry(self, textvariable=self.textvariable).grid(row=1, column=0, sticky='nsew')
         self.grid_columnconfigure(0, weight=1)
-        Button(self, text='Find', command=self.on_button_find_clicked).grid(row=1, column=1, columnspan=2, sticky='nsew')
+        Button(self, text='Find', command=self.on_button_find_clicked).grid(row=1, column=1, sticky='nsew')
         Label(self, text='Definition:').grid(row=2, column=0, sticky='w')
         self.create_text_widget(self.definition, row=3, column=0, columnspan=2)
         Label(self, text='Additional information:').grid(row=4, column=0, sticky='w')
         self.create_text_widget(self.additional_info, row=5, column=0, columnspan=2)
-        Button(self, text='Save', command=self.on_button_save_clicked).grid(row=6, column=1, sticky='nsew')
         self.create_table_view()
         self.create_table_switch_buttons()
+        frame = Frame(self)
+        frame.grid(row=6, column=0, columnspan=2, sticky='nsew')
+        frame.grid_rowconfigure(0, weight=1)
+        for column in range(4):
+            frame.grid_columnconfigure(column, weight=1)
+        Button(frame, text='Test').grid(row=0, column=0, sticky='nsew')
+        Button(frame, text='Delete', command=self.on_button_delete_clicked).grid(row=0, column=1, sticky='nsew')
+        Button(frame, text='Edit', command=self.on_button_edit_clicked).grid(row=0, column=2, sticky='nsew')
+        Button(frame, text='Save', command=self.on_button_save_clicked).grid(row=0, column=3, sticky='nsew')
 
     def create_text_widget(self, text_widget, row=0, column=0, columnspan=0):
         text_widget.grid(row=row, column=column, columnspan=columnspan)
@@ -72,53 +80,90 @@ class View(Tk):
         for item in items:
             self.table.delete(item)
         self.textvariable.set('')
-        self.clear_definition()
+        self.definition.delete('1.0', 'end')
         self.additional_info.delete('1.0', 'end')
+        self.current_records.clear()
 
     def on_button_find_clicked(self):
-        self.clear_definition()
-        word = self.textvariable.get()
+        word = self.textvariable.get().lower()
         if not word:
             tmb.showwarning('Warning', 'Word entry is empty')
-            self.definition.config(state='disabled')
             return
+        if self.record_exists(word):
+            self.check_item_and_update_view(word)
+            return
+        self.clear_all()
+        self.textvariable.set(word)
         status, result = self.controller.get_word_definition(word)
         if status:
             for word_type in result:
-                self.insert_to_definition(word_type + '\n')
+                self.definition.insert('insert', word_type + '\n')
                 for num, text in enumerate(result[word_type], 1):
-                    self.insert_to_definition(f'{num}) {text}\n')
+                    self.definition.insert('insert', f'{num}) {text}\n')
         else:
             tmb.showerror('Error', result)
 
-    def clear_definition(self):
-        self.definition.config(state='normal')
-        self.definition.delete('1.0', 'end')
-        self.definition.config(state='disabled')
+    def record_exists(self, word):
+        return self.db_manager.record_exists(self.get_tablename(word), word)
 
-    def insert_to_definition(self, text):
-        self.definition.config(state='normal')
-        self.definition.delete('1.0', 'end')
-        self.definition.insert('insert', text)
-        self.definition.config(state='disabled')
+    def check_item_and_update_view(self, word):
+        self.view_table(self.get_tablename(word))
+        self.select_existing_record(word)
+
+    def select_existing_record(self, word):
+        children = self.table.get_children()
+        for child in children:
+            if word == self.table.item(child, 'text'):
+                self.table.focus(child)
+                self.table.event_generate('<ButtonRelease-1>')
+                self.table.selection_set(child)
 
     def on_button_save_clicked(self):
-        table_name = self.textvariable.get()[0]
-        parameters = (self.textvariable.get(),
-                      self.retrieve_text(self.definition),
-                      self.retrieve_text(self.additional_info),
-                      0.0)
-        self.db_manager.save_record(table_name, parameters)
+        word = self.textvariable.get()
+        definition = self.retrieve_text(self.definition)
+        additional_info = self.retrieve_text(self.additional_info)
+        score = 0.0
+        if not word or not definition:
+            tmb.showwarning('Warning', 'Not all required fields are filled')
+            return
+        parameters = (word, definition, additional_info, score)
+        self.db_manager.save_record(self.get_tablename(word), parameters)
+        self.check_item_and_update_view(word)
+
+    def on_button_edit_clicked(self):
+        if not self.table.focus():
+            return
+        word = self.table.item(self.table.selection())['text']
+        score = self.table.item(self.table.selection())['values'][0]
+        definition = self.retrieve_text(self.definition)
+        additional_info = self.retrieve_text(self.additional_info)
+        if not definition:
+            tmb.showwarning('Warning', 'Definition must be filled')
+            return
+        parameters = (definition, additional_info, score)
+        self.db_manager.update_record(self.get_tablename(word), word, parameters)
+        self.check_item_and_update_view(word)
+
+    def on_button_delete_clicked(self):
+        if not self.table.focus():
+            return
+        word = self.table.item(self.table.selection())['text']
+        self.db_manager.delete_record(self.get_tablename(word), word)
+        self.view_table(self.get_tablename(word))
 
     def retrieve_text(self, text_widget):
         return text_widget.get('1.0', 'end')
+
+    def get_tablename(self, word):
+        return word[0]
 
     def view_selected_item(self, item):
         current_item = self.table.focus()
         word = self.table.item(current_item)['text']
         for record in self.current_records:
             if record[0] == word:
-                self.insert_to_definition(record[1])
+                self.definition.delete('1.0', 'end')
+                self.definition.insert('insert', record[1])
                 self.additional_info.delete('1.0', 'end')
                 self.additional_info.insert('insert', record[2])
 
